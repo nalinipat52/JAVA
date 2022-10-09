d85d3c00f512e88eaaf065bc12e3d7af99528da6
Nalini <nalini.pat52@gmail.com>
2022-10-09 10:12:23 +0530

    Looping

---
+++ Loop_P/while2.java
@@ -0,0 +1,12 @@
+package Loop_P;
+
+public class while2 {
+    public static void main(String[] args) {
+        int n = 100;
+        while(n!=0)
+        {
+            System.out.print(" "+n);
+            n = n-5;
+        }
+    }
+}
